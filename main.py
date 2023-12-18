3ac8714647f29c3aa1095c3e71880c71b6f46100
Sam Piper <sampii@protonmail.com>
2023-12-18 01:36:54 +0000

    fix: global archive

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from dotenv import load_dotenv
 import json
 import os
 
+# Load environment variables
 load_dotenv()
 
 
+# Load playlists from a JSON file
 def load_playlists(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
 
-# Set the parent directory and FFmpeg path directly
+# Set the parent directory, FFmpeg path, and JSON path
 parent_dir = os.getenv('PARENT_DIR')
 ffmpeg_path = os.getenv('FFMPEG_PATH')
 json_path = os.getenv('JSON_PATH')
 
-# Map directories to playlist URLs
+# Load playlists
 playlists = load_playlists(json_path)
+
+# Global archive file for tracking all downloads
+global_archive_file = os.path.join(parent_dir, 'global_archive.txt')
 
 
 # Function to process and download each video with metadata
@@ -35,14 +40,13 @@ def process_video(video_info, target_dir, base_ydl_opts):
     try:
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_info["webpage_url"]])
+        print(f"Downloaded video: {video_info['title']}")
     except Exception as e:
         print(f"Error downloading video '{video_info['title']}': {e}")
 
 
 # Download a playlist with metadata for each video
-def download_playlist(playlist_url, target_dir, playlist_name, base_ydl_opts):
-    archive_file = os.path.join(parent_dir, f"{playlist_name}_archive.txt")
-
+def download_playlist(playlist_url, target_dir, base_ydl_opts):
     try:
         with YoutubeDL(base_ydl_opts) as ydl:
             playlist_info = ydl.extract_info(playlist_url, download=False)
@@ -50,10 +54,10 @@ def download_playlist(playlist_url, target_dir, playlist_name, base_ydl_opts):
                 if video_info:
                     process_video(video_info, target_dir, base_ydl_opts)
     except Exception as e:
-        print(f"Error processing playlist '{playlist_name}': {e}")
+        print(f"Error processing playlist: {e}")
 
 
-# Base download options
+# Base download options with a global archive file
 base_download_options = {
     "format": "bestaudio/best",
     "postprocessors": [
@@ -65,11 +69,11 @@ base_download_options = {
     "writethumbnail": True,
     "noplaylist": False,
     "ignoreerrors": True,
+    "download_archive": global_archive_file
 }
 
 # Ensure target directories exist and download each playlist
 for playlist_name, playlist_url in playlists.items():
     target_dir = os.path.join(parent_dir, playlist_name)
     os.makedirs(target_dir, exist_ok=True)
-    base_download_options["download_archive"] = os.path.join(parent_dir, f"{playlist_name}_archive.txt")
-    download_playlist(playlist_url, target_dir, playlist_name, base_download_options)
+    download_playlist(playlist_url, target_dir, base_download_options)
